339a1986cb7f59157f7f67754baa6d79d32abdd6
Tianyu Chen <sweetyfish@deepin.org>
2023-10-18 16:21:18 +0800

    feat: update node-schema-utils to 3.1.1_ds

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,25 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+### [3.1.1](https://github.com/webpack/schema-utils/compare/v3.1.0...v3.1.1) (2021-07-19)
+
+
+### Bug Fixes
+
+* update error message for `integer` ([#136](https://github.com/webpack/schema-utils/issues/136)) ([2daa97e](https://github.com/webpack/schema-utils/commit/2daa97eae87e6790b92711746a6a527b859ac13b))
+
+## [3.1.0](https://github.com/webpack/schema-utils/compare/v3.0.0...v3.1.0) (2021-06-30)
+
+
+### Features
+
+* added the `link` property in validation error ([589aa59](https://github.com/webpack/schema-utils/commit/589aa5993424a8bc45ec22b67dff55be92c456a9))
+
+
+### Bug Fixes
+
+* non-empty validation error message ([#116](https://github.com/webpack/schema-utils/issues/116)) ([c51abef](https://github.com/webpack/schema-utils/commit/c51abefa4d4d62e1346b3a105182d36675595077))
+
 ## [3.0.0](https://github.com/webpack/schema-utils/compare/v2.7.1...v3.0.0) (2020-10-05)
 
 
--- src/validate.js
+++ src/validate.js
@@ -1,10 +1,10 @@
-import addAbsolutePathKeyword from './keywords/absolutePath';
+import addAbsolutePathKeyword from "./keywords/absolutePath";
 
-import ValidationError from './ValidationError';
+import ValidationError from "./ValidationError";
 
 // Use CommonJS require for ajv libs so TypeScript consumers aren't locked into esModuleInterop (see #110).
-const Ajv = require('ajv');
-const ajvKeywords = require('ajv-keywords');
+const Ajv = require("ajv");
+const ajvKeywords = require("ajv-keywords");
 
 /** @typedef {import("json-schema").JSONSchema4} JSONSchema4 */
 /** @typedef {import("json-schema").JSONSchema6} JSONSchema6 */
@@ -17,6 +17,7 @@ const ajvKeywords = require('ajv-keywords');
  * @property {number=} formatMaximum
  * @property {boolean=} formatExclusiveMinimum
  * @property {boolean=} formatExclusiveMaximum
+ * @property {string=} link
  */
 
 /** @typedef {(JSONSchema4 | JSONSchema6 | JSONSchema7) & Extend} Schema */
@@ -44,10 +45,10 @@ const ajv = new Ajv({
 });
 
 ajvKeywords(ajv, [
-  'instanceof',
-  'formatMinimum',
-  'formatMaximum',
-  'patternRequired',
+  "instanceof",
+  "formatMinimum",
+  "formatMaximum",
+  "patternRequired",
 ]);
 
 // Custom keywords
